a14b22d2f4342daaa134fd7cd39a1618130e41ee
Felipe Dias <dias20felipe@gmail.com>
2024-04-10 21:34:07 -0300

    navbar alterada

--- doacao.js
+++ doacao.js
@@ -1,19 +1,1 @@
 
-
-const mp = new MercadoPago('TEST-02340453-dbe2-44aa-b5b6-2a63c1ab16da');
-const bricksBuilder = mp.bricks();
-
-
-
-mp.bricks().create("wallet", "wallet_container", {
-    initialization: {
-        preferenceId: "<PREFERENCE_ID>",
-    },
- customization: {
-  texts: {
-   valueProp: 'smart_option',
-  },
-  },
- });
- 
-
